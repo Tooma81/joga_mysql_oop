4c10ec3c4e4952a2c6524da7c0c23b2f4b674e84
Tooma81 <toomas.kaarel@gmail.com>
2025-09-11 09:32:42 +0300

    Establish database connection

---
+++ utils/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
+
+const app = express()
+
+app.set('views', path.join(__dirname, '/../views'))
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/../views/layouts/'
+}))
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+module.exports = app
