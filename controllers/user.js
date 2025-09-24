34fd7bed3843fcd80b12443916874422afb658ff
Tooma81 <toomas.kaarel@gmail.com>
2025-09-24 11:20:05 +0300

    Added password checks

--- controllers/user.js
+++ controllers/user.js
@@ -9,22 +9,35 @@ class userController {
                 message: 'Username already taken'
             })
         } else {
-            const cryptPassword = await bcrypt.hash(req.body.password, 10)
-            const registeredId = await userModel.create({
-                username: req.body.username,
-                email: req.body.email,
-                password: cryptPassword
-            })
-            if(registeredId){
-                const userData = await userModel.findById(registeredId)
-                req.session.user = {
-                    username: userData.username,
-                    user_id: userData.id
-                }
+            // Password needs to be at least 6 characters
+            //  and cannot be 'qwerty', 'password' or a continious sequence of numbers
+            if(
+                !req.body.password ||
+                req.body.password.length < 6 || 
+                '1234567890'.includes(req.body.password) ||
+                ['qwerty', 'password'].includes(req.body.password.toLowerCase())
+            ){
                 res.json({
-                    message: 'New user is registered',
-                    user_session: req.session.user
+                    message: 'Invalid password'
                 })
+            } else {
+                const cryptPassword = await bcrypt.hash(req.body.password, 10)
+                const registeredId = await userModel.create({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: cryptPassword
+                })
+                if(registeredId){
+                    const userData = await userModel.findById(registeredId)
+                    req.session.user = {
+                        username: userData.username,
+                        user_id: userData.id
+                    }
+                    res.json({
+                        message: 'New user is registered',
+                        user_session: req.session.user
+                    })
+                }
             }
         }
     }
